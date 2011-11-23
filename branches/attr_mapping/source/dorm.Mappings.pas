f24b8dca07636351d9cb3715e310d56350c7208f
magnomp@gmail.com <magnomp@gmail.com@bb774d6e-3ff5-112e-3884-4813708f7849>
2011-11-23 12:26:17 +0000

    Implementation of CoC and attributes mapping.

--- branches/attr_mapping/source/dorm.Mappings.pas
+++ branches/attr_mapping/source/dorm.Mappings.pas
@@ -22,6 +22,10 @@ type
     property ColumnName: String read FColumnName;
   end;
 
+  PrimaryKey = class(TCustomAttribute)
+
+  end;
+
   IMappingStrategy = interface
     ['{C580ADCC-B58C-4BDC-AB19-D0E6A0B539D5}']
     function TableName(const AType: TRttiType): String;
@@ -47,7 +51,17 @@ type
     function TableName(const AType: TRttiType): String;
   end;
 
+  TCoCMappingStrategy = class(TInterfacedObject, IMappingStrategy)
+  private
+    function FieldName(const AProperty: TRttiProperty): string;
+    function PKPropertyName(const AType: TRttiType): string;
+    function TableName(const AType: TRttiType): string;
+  end;
+
 implementation
+
+uses
+  SysUtils;
 
 { TDelegateMappingStrategy }
 
@@ -123,14 +137,24 @@ var
 begin
   Result := Nil;
   for Attr in Obj.GetAttributes do
-    if Attr is T then
+    if Attr.ClassType.InheritsFrom(T) then
       Result := T(Attr);
 end;
 
 function TAttributesMappingStrategy.PKPropertyName(
   const AType: TRttiType): String;
+var
+  LProperty: TRttiProperty;
+  Properties: TArray<TRttiProperty>;
+  i: Integer;
 begin
-
+  Properties := AType.GetProperties;
+  for i := 0 to High(Properties) do
+  begin
+    LProperty := Properties[i];
+    if Assigned(GetAttribute<PrimaryKey>(LProperty)) then
+      Result := LProperty.Name;
+  end;
 end;
 
 function TAttributesMappingStrategy.TableName(const AType: TRttiType): String;
@@ -146,6 +170,27 @@ end;
 constructor Column.Create(const ColumnName: String);
 begin
   FColumnName := ColumnName;
+end;
+
+{ TCoCMappingStrategy }
+
+function TCoCMappingStrategy.FieldName(const AProperty: TRttiProperty): string;
+begin
+  Result := AnsiUpperCase(AProperty.Name);
+end;
+
+function TCoCMappingStrategy.PKPropertyName(const AType: TRttiType): string;
+begin
+  //TODO: We have to define a good convention for primary key
+  Result := '';
+end;
+
+function TCoCMappingStrategy.TableName(const AType: TRttiType): string;
+begin
+  if AType.Name[1] = 'T' then
+    Result := AnsiUpperCase(Copy(AType.Name, 2))
+  else
+    Result := AnsiUpperCase(AType.Name);
 end;
 
 end.
